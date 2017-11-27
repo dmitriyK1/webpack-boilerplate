527c4bb204299e92c1442e724b05f4dda682d12f
Maksym Boudnik <maksym.boudnik@nextiva.com>
2017-11-27 14:03:43 +0200

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
 
 const config = {
-	entry: './index',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader'
-			}
-		]
-	}
+  entry: './index',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }
+    ]
+  }
 };
 
 module.exports = config;
